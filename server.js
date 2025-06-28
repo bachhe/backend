1f2f17371a300c1bd2223c3c937d79c9802bfd6e
AyushNoX <mail@ayush-singh.in>
2025-06-28 15:26:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://stream11.vercel.app',
   credentials: true
 }));
 
